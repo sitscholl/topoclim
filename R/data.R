709cfc74a3baf4fc90a47b72601c088390fcc86c
sitscholl <tscholl.simon@gmail.com>
2021-01-19 08:23:41 +0100

    changed to sp and data.frame

--- R/data.R
+++ R/data.R
@@ -6,16 +6,29 @@
 #' \describe{
 #' \item{st_id}{id of the station}
 #' \item{date}{measurement date}
-#' \item{tmean}{daily mean temperature}
-#' \item{irrad}{daily solar irradiation}
+#' \item{tmean}{daily mean temperature in deg C}
+#' \item{irradiation}{daily solar irradiation in kWh/m2}
 #' }
 "timeseries"
 
+#' Table with daily observations of solar irradiation for a long time period.
+#'
+#' A data.frame containing a column for station id, date and daily solar irradiation
+#'
+#' @format A data frame with 16425 rows and 4 variables:
+#' \describe{
+#' \item{st_id}{id of the station}
+#' \item{date}{measurement date}
+#' \item{month}{measurement month}
+#' \item{irradiation}{daily solar irradiation in kWh/m2}
+#' }
+"rad_longterm"
+
 #' Location of the official stations
 #'
-#' A shapefile with the location of the official meteorological stations from the national meteorological service
+#' A SpatialPointsDataFrame with the location of the official meteorological stations from the national meteorological service
 #'
-#' @format An sf object with 15 features and 4 variables:
+#' @format A SpatialPointsDataFrame with 15 features and 4 variables:
 #' \describe{
 #' \item{st_id}{id of the station}
 #' \item{elev}{elevation of the station in meters above sea level}
